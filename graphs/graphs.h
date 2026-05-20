13c1a4b90afbd2784ece28c8483c7ff944e89afd
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-05-20 19:06:27 +0200

    Refactor graph header with enqueue and process_node

--- graphs/graphs.h
+++ graphs/graphs.h
@@ -1,46 +1,101 @@
-#ifndef GRAPHS_H
-#define GRAPHS_H
+#include <stdlib.h>
+#include "graphs.h"
 
-#include <stddef.h>
-
-typedef enum edge_type_e
+/**
+ * enqueue - Adds a vertex to the tail of the queue
+ *
+ * @head: Pointer to the head of the queue
+ * @tail: Pointer to the tail of the queue
+ * @v: Vertex to enqueue
+ * @depth: Depth of the vertex
+ *
+ * Return: 1 on success, 0 on failure
+ */
+static int enqueue(queue_node_t **head, queue_node_t **tail,
+		const vertex_t *v, size_t depth)
 {
-	UNIDIRECTIONAL = 0,
-	BIDIRECTIONAL
-} edge_type_t;
+	queue_node_t *node;
 
-typedef struct vertex_s vertex_t;
+	node = malloc(sizeof(queue_node_t));
+	if (!node)
+		return (0);
+	node->v = v;
+	node->depth = depth;
+	node->next = NULL;
+	if (!*head)
+		*head = node;
+	else
+		(*tail)->next = node;
+	*tail = node;
+	return (1);
+}
 
-typedef struct edge_s
+/**
+ * process_node - Dequeues a node, calls action, enqueues unvisited neighbors
+ *
+ * @head: Pointer to the head of the queue
+ * @tail: Pointer to the tail of the queue
+ * @visited: Array tracking visited vertex indices
+ * @action: Function to call on each visited vertex
+ * @max_depth: Pointer to the maximum depth reached
+ */
+static void process_node(queue_node_t **head, queue_node_t **tail,
+		char *visited, void (*action)(const vertex_t *, size_t),
+		size_t *max_depth)
 {
-	vertex_t	*dest;
-	struct edge_s	*next;
-} edge_t;
+	queue_node_t *tmp;
+	edge_t *edge;
 
-struct vertex_s
+	tmp = *head;
+	*head = (*head)->next;
+	if (!*head)
+		*tail = NULL;
+	action(tmp->v, tmp->depth);
+	if (tmp->depth > *max_depth)
+		*max_depth = tmp->depth;
+	edge = tmp->v->edges;
+	while (edge)
+	{
+		if (!visited[edge->dest->index])
+		{
+			visited[edge->dest->index] = 1;
+			enqueue(head, tail, edge->dest, tmp->depth + 1);
+		}
+		edge = edge->next;
+	}
+	free(tmp);
+}
+
+/**
+ * breadth_first_traverse - Traverses a graph using breadth-first algorithm
+ *
+ * @graph: Pointer to the graph to traverse
+ * @action: Function to call for each visited vertex
+ *
+ * Return: The biggest vertex depth reached, or 0 on failure
+ */
+size_t breadth_first_traverse(const graph_t *graph,
+		void (*action)(const vertex_t *v, size_t depth))
 {
-	size_t		index;
-	char		*content;
-	size_t		nb_edges;
-	edge_t		*edges;
-	struct vertex_s	*next;
-};
+	queue_node_t *head, *tail;
+	char *visited;
+	size_t max_depth;
 
-typedef struct graph_s
-{
-	size_t		nb_vertices;
-	vertex_t	*vertices;
-} graph_t;
-
-/* Function prototypes */
-graph_t		*graph_create(void);
-vertex_t	*graph_add_vertex(graph_t *graph, const char *str);
-int		graph_add_edge(graph_t *graph, const char *src,
-			const char *dest, edge_type_t type);
-void		graph_delete(graph_t *graph);
-size_t		depth_first_traverse(const graph_t *graph,
-			void (*action)(const vertex_t *v, size_t depth));
-size_t		breadth_first_traverse(const graph_t *graph,
-			void (*action)(const vertex_t *v, size_t depth));
-
-#endif /* GRAPHS_H */
+	if (!graph || !action || !graph->vertices)
+		return (0);
+	visited = calloc(graph->nb_vertices, sizeof(char));
+	if (!visited)
+		return (0);
+	head = tail = NULL;
+	max_depth = 0;
+	if (!enqueue(&head, &tail, graph->vertices, 0))
+	{
+		free(visited);
+		return (0);
+	}
+	visited[graph->vertices->index] = 1;
+	while (head)
+		process_node(&head, &tail, visited, action, &max_depth);
+	free(visited);
+	return (max_depth);
+}
