f0f733ba27d148c398fe7bf990d89f01cd385a6b
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-05-31 21:47:38 +0200

    Update huffman.h

--- huffman_coding/huffman.h
+++ huffman_coding/huffman.h
@@ -20,4 +20,7 @@ int huffman_extract_and_insert(heap_t *priority_queue);
 /* huffman_tree.c */
 binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size);
 
+/* huffman_codes.c */
+int huffman_codes(char *data, size_t *freq, size_t size);
+
 #endif /* HUFFMAN_H */
