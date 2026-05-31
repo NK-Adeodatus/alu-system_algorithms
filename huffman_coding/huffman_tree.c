964f66754fa6b6c4d371b5595e34736364379971
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-05-31 21:30:01 +0200

    Create huffman_tree.c

---
+++ huffman_coding/huffman_tree.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "huffman.h"
+
+/**
+ * huffman_tree - Builds the Huffman tree
+ *
+ * @data: Array of characters
+ * @freq: Array of associated frequencies
+ * @size: Size of both arrays
+ *
+ * Return: Pointer to the root node of the Huffman tree, or NULL if it fails
+ */
+binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
+{
+	heap_t *priority_queue;
+	binary_tree_node_t *root;
+
+	if (data == NULL || freq == NULL || size == 0)
+		return (NULL);
+
+	priority_queue = huffman_priority_queue(data, freq, size);
+	if (priority_queue == NULL)
+		return (NULL);
+
+	while (priority_queue->size > 1)
+	{
+		if (!huffman_extract_and_insert(priority_queue))
+		{
+			heap_delete(priority_queue, free);
+			return (NULL);
+		}
+	}
+
+	root = (binary_tree_node_t *)heap_extract(priority_queue);
+
+	free(priority_queue);
+
+	return (root);
+}
